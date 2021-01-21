503ebf97f9fca6bc853c87906d9b967df62f4c65
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-21 01:46:11 -0500

    Add 404 Page

---
+++ components/layout/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+const Error404 = () => {
+    return ( <h1
+        css={css`
+                    margin-top: 5rem;
+                    text-align: center;
+                `}
+    >404 Not Found</h1> );
+}
+
+export default Error404;
