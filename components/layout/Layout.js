085273e4da4cd190e64abff95c7f1a88a902568a
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-20 23:25:30 -0500

    Move Layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from "next/head";
+
+const Layout = props => {
+    return (
+        <>
+            <Head>
+                <title>Product Hunt</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+                      integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+                      crossOrigin="anonymous"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+                    rel="stylesheet"/>
+            </Head>
+            <main>
+                {props.children}
+            </main>
+        </>
+    );
+};
+
+export default Layout;
