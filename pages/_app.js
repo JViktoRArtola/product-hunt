1877e47a9958184d67f8106f2124e3b4d9f6c6b2
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-21 01:08:33 -0500

    Add Firebase Context.Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import firebase, {FirebaseContext} from "../firebase/";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}) {
+    return (
+        <FirebaseContext.Provider value={{firebase}}>
+            <Component {...pageProps}  />
+        </FirebaseContext.Provider>
+    )
 }
 
 export default MyApp
