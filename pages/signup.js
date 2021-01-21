4ba3fb4c744b301094e00c17d12e3580cb825e3a
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-21 00:55:15 -0500

    Add Validation Hook

--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,20 @@
 import React, {useState} from 'react';
 import Layout from "../components/layout/Layout";
-import {Error, Field, Form, InputSubmit} from "../components/ui/Form";
 import {css} from "@emotion/react";
+import {Error, Field, Form, InputSubmit} from "../components/ui/Form";
+import useValidation from "../hooks/useValidation";
+import validateSignUp from "../validations/validateSignUp";
+
+const STATE_INICIAL = {
+    name: '',
+    email: '',
+    password: ''
+}
 
 const Signup = () => {
     const [error, setError] = useState(false);
+    const {values, errors, handleSubmit, handleChange, handleBlur} = useValidation(STATE_INICIAL, validateSignUp)
+    const {name, email, password} = values
     return (
         <div>
             <Layout>
@@ -13,6 +23,7 @@ const Signup = () => {
                   margin-top: 5rem;
             `}> Sign Up </h1>
                 <Form
+                    onSubmit={handleSubmit}
                     noValidate
                 >
                     <Field>
@@ -22,9 +33,12 @@ const Signup = () => {
                             id="name"
                             placeholder="Your Name"
                             name="name"
+                            value={name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                         />
                     </Field>
-                    {/*{errors.name && <Error>{errors.name}</Error> }*/}
+                    {errors.name && <Error>{errors.name}</Error> }
                     <Field>
                         <label htmlFor="email">Email</label>
                         <input
@@ -32,9 +46,12 @@ const Signup = () => {
                             id="email"
                             placeholder="Your Email"
                             name="email"
+                            value={email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                         />
                     </Field>
-                    {/*{errors.email && <Error>{errors.email}</Error> }*/}
+                    {errors.email && <Error>{errors.email}</Error> }
                     <Field>
                         <label htmlFor="password">Password</label>
                         <input
@@ -42,9 +59,12 @@ const Signup = () => {
                             id="password"
                             placeholder="Password"
                             name="password"
+                            value={password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
                         />
                     </Field>
-                    {/*{errors.password && <Error>{errors.password}</Error> }*/}
+                    {errors.password && <Error>{errors.password}</Error> }
                     {error && <Error>{error}</Error> }
                     <InputSubmit
                         type="submit"
